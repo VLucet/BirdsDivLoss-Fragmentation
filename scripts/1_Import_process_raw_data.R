2dbbaad24870a774e55de872852f9b173ef00c98
Valentin <valentin.lucet@gmail.com>
2020-10-23 11:33:14 -0400

    updates lock file, start data cleaning

---
+++ scripts/1_Import_process_raw_data.R
@@ -0,0 +1,8 @@
+##### LDP Project ~ Assignment 2 ~ October 2020
+##### Script 1 ~ Import & Process raw data
+
+# Load Packages -----------------------------------------------------------
+
+library(raster) # For spatial operations
+library(ncdf4) # For loading NetCDF data
+library(rgeobon) # For accessing EBV data
