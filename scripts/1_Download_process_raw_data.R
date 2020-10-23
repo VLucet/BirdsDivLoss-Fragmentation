37ea582221d0168a930d32135478a489269719f9
Valentin <valentin.lucet@gmail.com>
2020-10-23 13:18:29 -0400

    updates

--- scripts/1_Download_process_raw_data.R
+++ scripts/1_Download_process_raw_data.R
@@ -14,6 +14,20 @@ dataset_list <- geobon_list()
 View(dataset_list) # We need dataset 1 and 4
 
 # Download data in data/raw
-# Dataset 1 is bird data and is very light
-# Dataset 4 is fragmentation data, and is quite heavy
+# Dataset 1 is bird data and is 1.5 MB.
+# Dataset 4 is fragmentation data, and is 12 GB.
 geobon_download(id = c(1,4), path = "data/raw")
+
+# Import data -------------------------------------------------------------
+
+# Bird data 
+# Print info
+print(nc_open("data/raw/cSAR_idiv_v1.nc"))
+# Import as raster brick
+bird <- brick("data/raw/cSAR_idiv_v1.nc")
+# Change the naming of columns
+names(bird) <- paste0("change_", c(seq(from=1910, to=2010, by=10), 2015))
+
+# Fragmentation data
+
+         
