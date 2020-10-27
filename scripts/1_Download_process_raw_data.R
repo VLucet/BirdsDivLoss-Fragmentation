77a93b8e1609a8b7a952485c6a2296b3409d6818
Valentin <valentin.lucet@gmail.com>
2020-10-27 15:48:19 -0400

    updates

--- scripts/1_Download_process_raw_data.R
+++ scripts/1_Download_process_raw_data.R
@@ -3,7 +3,8 @@
 
 # Load Packages -----------------------------------------------------------
 
-library(raster) # For spatial operations
+library(raster) # For spatial operations on grid data
+library(sf) # For spatial operations on vector data
 library(ncdf4) # For loading NetCDF data
 library(rgeobon) # For accessing EBV data
 
@@ -23,11 +24,32 @@ geobon_download(id = c(1,4), path = "data/raw")
 # Bird data 
 # Print info
 print(nc_open("data/raw/cSAR_idiv_v1.nc"))
-# Import as raster brick
+# Import as raster brick and set epsg correctly
 bird <- brick("data/raw/cSAR_idiv_v1.nc")
+crs(bird) <- crs("+proj=longlat +datum=WGS84 +no_defs")
 # Change the naming of columns
 names(bird) <- paste0("change_", c(seq(from=1910, to=2010, by=10), 2015))
+# Get most recent layer
+bird_2015 <- bird$change_2015
 
 # Fragmentation data
+# Print info
+RMF <- (nc_open("data/raw/RMF_002.nc"))
+# Import as raster brick and set epsg correctly
+RMF <- brick("data/raw/RMF_002.nc")
+crs(RMF) <- crs("+proj=longlat +datum=WGS84 +no_defs")
+# Get most recent layer
+RMF_2018 <- RMF$X2018.01.01
 
+# Canada Ecodistricts
+# Downloaded the GML here 
+# https://open.canada.ca/data/en/dataset/fe9fd41c-1f67-4bc5-809d-05b62986b26b
+ecodist <- st_read("data/raw/BIO_CA_TER_ECODISTRICT_V2_2/BIO_CA_TER_ECODISTRICT_V2_2.gml")
          
+# Process data ------------------------------------------------------------
+
+writeRaster(RMF_2018, "test.tif")
+
+# Crop data
+bird_2015_ca <- crop(bird_2015, st_transform(ecodist, crs(bird_2015)))
+RMF_2018_ca <- crop(RMF_2018, st_transform(ecodist, crs(RMF_2018)))
